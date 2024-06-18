43c7f42b6e819c191c134493cd0effc1ee5af476
Marcos Dorado Muñoz <38770399+Doradux@users.noreply.github.com>
2024-06-18 23:21:44 +0200

    Update DB connection

--- Model/DBconection.php
+++ Model/DBconection.php
@@ -1,26 +1,35 @@
 <?php
+
 if (session_status() == PHP_SESSION_NONE) session_start();
+if (isset($_GET['join']) && isset($_SESSION['currentUser']['username'])) {
+?>
 
-abstract class DBconection
-{
-    private static $server = 'localhost';
-    private static $db = 'cex';
-    private static $user = 'root';
-    private static $password = '';
+    <script>
+        var serverId = "<?= $_GET['join'] ?>";
+        console.log("received join id: <?= $_GET['join'] ?>");
 
-    public static function connectDB()
-    {
-        try {
-            // Use utf8mb4 charset for full Unicode support, including emojis
-            $connection = new PDO("mysql:host=" . self::$server . ";dbname=" . self::$db . ";charset=utf8mb4", self::$user, self::$password);
-            // Set the PDO attribute to handle errors properly
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // Ensure that the connection uses utf8mb4
-            $connection->exec("SET NAMES 'utf8mb4'");
-        } catch (PDOException $e) {
-            echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
-            die("Error: " . $e->getMessage());
-        }
-        return $connection;
-    }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                if (response.status == "success") {
+                    window.location = "./home.php"
+                    console.log(response);
+                } else {
+                    console.error(response);
+                    alert("Something bad happend: check console")
+                }
+            }
+        };
+
+        xhttp.open("POST", "../controller/jsToPhp/joinServer.php", true);
+        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        var params = "serverId=" + encodeURIComponent(serverId);
+        xhttp.send(params);
+    </script>
+
+<?php
+} else if (isset($_SESSION['currentUser']['username']) && !isset($_GET['join'])) {
+    header('Location: ./home.php');
 }
+include './controller/index.php';
